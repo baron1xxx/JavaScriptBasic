64076424aec8dbb59c713773a26d0ecc531fdace
baron1xxxx <baron1xxx@gmail.com>
2020-04-09 18:54:34 +0300

    create stringBoolean file

--- dataTypes/index.js
+++ dataTypes/index.js
@@ -1,4 +1,5 @@
 // module.exports.dataTypes = require('./dataTypes');
 // module.exports.numberString = require('./numberString');
 // module.exports.numberBoolean = require('./numberBoolean');
-module.exports.NumberNullUndefined = require('./NumberNullUndefined');
+// module.exports.NumberNullUndefined = require('./NumberNullUndefined');
+module.exports.stringBoolean = require('./stringBoolean');
---
+++ dataTypes/stringBoolean.js
@@ -0,0 +1,24 @@
+// console.log('string' + true);// stringtrue
+// console.log(typeof ('string' + true));// string
+//
+// console.log('string' + false);// stringfalse
+// console.log(typeof ('string' + false));// string
+
+// console.log('string' - true);// NaN
+// console.log(typeof ('string' - true));// number
+//
+// console.log('string' - false);// NaN
+// console.log(typeof ('string' - false));// number
+
+// console.log('string' * true);// NaN
+// console.log('' * true);// 0
+// console.log(typeof ('string' * true));// number
+//
+// console.log('string' * false);// NaN
+// console.log(typeof ('string' * false));// number
+
+// console.log('string' / true);// NaN
+// console.log(typeof ('string' / true));// number
+//
+// console.log('string' / false);// NaN
+// console.log(typeof ('string' / false));// number
