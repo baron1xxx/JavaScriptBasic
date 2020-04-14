d61d79048c903a4973354123ca6e0eb110142e38
baron1xxxx <baron1xxx@gmail.com>
2020-04-14 19:26:02 +0300

    Add toUpperCase file

--- String/toUpperCase.js
+++ String/toUpperCase.js
@@ -1,3 +1,5 @@
 let str = 'Hello WORD';
 console.log(str.toUpperCase()); // 'HELLO WORD'
 console.log(str);// 'Hello WORD' - не міняє початкової строки.
+
+console.log('bnnhmghm'.toUpperCase()); // 'BNNHMGHM'
