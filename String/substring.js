65da31b391951c33b88895d0ada8238813e020f2
baron1xxxx <baron1xxx@gmail.com>
2020-04-14 19:19:01 +0300

    Add substring file

---
+++ String/substring.js
@@ -0,0 +1,17 @@
+/**
+ * @param indexA
+ * @param indexB
+ *return string
+ */
+
+// str.substring(indexA, indexB)
+let str1 = 'Hello word!';
+console.log(str1);
+str1.substring(0, 4);// 'Hell'
+console.log(str1);// Повертає нову строку неміняючи початкової.
+
+
+console.log('Hello word!'.substring(0, 4));// 'Hell'
+
+
+
