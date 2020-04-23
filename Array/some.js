a073bd0f5160221399b407a089c2f6d7073b6a29
baron1xxxx <baron1xxx@gmail.com>
2020-04-23 20:45:24 +0300

    Add some file

--- Array/some.js
+++ Array/some.js
@@ -1,11 +1,13 @@
 /**
+ * Перевіряє чи хочаб один елемент масиву задовільняє умову у callback.
  *@param callback (
  * element - елемент масиву під час ітераціїб
  *  index - індекс масиву під час ітераціїб
  *  array - сам масив) - перевіряє чи хочаб один елемент масиву відповідає заданій умові.
+ *@param thisArg - значення яке буде використовуватись як this у callback.
  *  return boolean
  */
-// arr.some(callback(element, index, array)
+// arr.some(callback(element, index, array, thisArg)
 
 let arr1 = [6, 12, '15', 100];
 
@@ -28,3 +30,13 @@ const arr4 = arr3.every((element, index, array) => {
     return typeof element === 'number'
 });
 console.log(arr4); // false
+
+
+// Чи біжить до кінця масиву чи зупиняється при задовільненні умови
+
+const array2 = [1,'Hello', 2, 3,'5', {name: 'Roman'}, 'Hello', 'word' ];
+const filter = array2.some((el) => {
+    console.log(el);
+    return el === 'Hello'
+});
+console.log(filter);
