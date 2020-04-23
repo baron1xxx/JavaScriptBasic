3fc461639d25293c36b0dd23637a14c1c76b1c3e
baron1xxxx <baron1xxx@gmail.com>
2020-04-23 13:32:42 +0300

    Add reverse file

---
+++ Array/reverse.js
@@ -0,0 +1,11 @@
+/**
+ * Перевертає масив(міняє послідовність елементів на протилежну).
+ * return array
+ */
+
+// arr.reverse()
+
+let arr = ['a', 'b', 'c'];
+const reverseArray = arr.reverse();
+console.log(reverseArray); // [ 'c', 'b', 'a' ]
+console.log(arr); // [ 'c', 'b', 'a' ]
