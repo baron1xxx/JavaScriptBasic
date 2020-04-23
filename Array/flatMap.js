6e5e5febe1f7afbafe585cdc6fea7945127395a0
baron1xxxx <baron1xxx@gmail.com>
2020-04-23 10:32:34 +0300

    Add flatMap file

---
+++ Array/flatMap.js
@@ -0,0 +1,28 @@
+/**
+ * Спочатку виконує callback на кожному елементі масива а потім вирівнює його до глибини 1!!!.
+ *@param callback (
+ *  element - елемент масиву під час ітераціїб
+ *  index - індекс масиву під час ітераціїб
+ *  array - сам масив) - перевіряє чи хочаб один елемент масиву відповідає заданій умові.
+ *@param thisArg - значення яке буде використовуватись як this у callback.
+ *  return new array.
+ */
+
+// arr.flatMap(callback(currentValue, index, array). this.Arg)
+
+let arr = [1,2,3, [4,5]];
+let thisArg = [];
+
+const flatMapArray = arr.flatMap((el, index, array) => {
+    return el;
+});
+console.log(flatMapArray);
+
+let arr1 = ["Сьогодні сонячно у", "", "Львові"];
+const strings = arr1.flatMap((el) => {
+    thisArg.push(el)
+    return el.split(' ')
+}, thisArg);
+console.log(strings);
+console.log('************');
+console.log(thisArg);
