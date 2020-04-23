960908f60a6cb542c6dab4f8ddef73e882cfbb90
baron1xxxx <baron1xxx@gmail.com>
2020-04-23 20:54:24 +0300

    changed description @param thisArg

--- Array/every.js
+++ Array/every.js
@@ -1,8 +1,10 @@
 /**
+ * Перевіряє чи кожен елемент масиву задовільняє умову у callback.
  *@param callback (
  * element - елемент масиву під час ітераціїб
  *  index - індекс масиву під час ітераціїб
  *  array - сам масив) - перевіряє кожен елемент масиву чи він відповідає заданій умові.
+ *@param thisArg - значення яке буде використовуватись як this у callback.
  *  return boolean
  */
 // arr.every(callback(element, index, array)
@@ -18,7 +20,7 @@ const arr2 = arr1.every((element, index, array) => {
 });
 console.log(arr2); // false
 
-let arr3 = ['Hello','6', '12', '15', '100'];
+let arr3 = ['Hello', '6', '12', '15', '100'];
 
 const arr4 = arr3.every((element, index, array) => {
     console.log(element);
@@ -28,3 +30,13 @@ const arr4 = arr3.every((element, index, array) => {
     return typeof element === 'string'
 });
 console.log(arr4); // true
+
+
+// Чи біжить до кінця масиву чи зупиняється при задовільненні умови
+
+const array2 = [1,'Hello', 2, 3,'5', {name: 'Roman'}, 'Hello', 'word' ];
+const filter = array2.every((el) => {
+    console.log(el);
+    return el === 'Hello'
+});
+console.log(filter);
