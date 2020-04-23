2c822ba1f978ef55d1399250da994adaf519cb9b
baron1xxxx <baron1xxx@gmail.com>
2020-04-23 13:14:16 +0300

    Add push file

---
+++ Array/push.js
@@ -0,0 +1,11 @@
+/**
+ * Додає елементи в укінець масива.
+ * return нову довжину масиву.
+ */
+
+// arr.push(element1, ..., elementN)
+let arr = ['a', 'b', 'c'];
+
+console.log(arr.push('d', 'e')); // 5.
+console.log(arr);// [ 'a', 'b', 'c', 'd', 'e' ]
+console.log([].push('d', 'e')); // 2.
