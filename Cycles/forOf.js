98c70ccbf064a22bef2d27ea6559eb18df001a81
baron1xxxx <baron1xxx@gmail.com>
2020-04-27 20:51:07 +0300

    forOf from object

--- Cycles/forOf.js
+++ Cycles/forOf.js
@@ -1,52 +1,29 @@
-class Credentials {
-    constructor(login, password) {
-        this.login = login
-        this.password = password
+let arr = [1, 2, '3', 4,{ name: 'Roman'}, 6, , 'Hello'];
+
+
+for (let arrElement of arr) {
+    if (!arrElement) {
+        break // Перериває цикл.
     }
-
+    console.log(arrElement);
 }
+// 1
+// 2
+// 3
+// 4
+// { name: 'Roman' }
+// 6
 
-class RegisterData extends Credentials{
-
-    constructor(login, password, firstName, lastName) {
-        super(login, password);
-        this.firstName = firstName;
-        this.lastName = lastName;
+for (let element of arr) {
+    if (element === 6) {
+        continue // Пропускає ітерацію.
     }
+    console.log(element);
 }
-
-class UserData extends RegisterData{
-
-    constructor(login, password, firstName, lastName, tel, avatar) {
-        super(login, password, firstName, lastName);
-        this.tel = tel;
-        this.avatar = avatar;
-    }
-}
-
-const user = new UserData('baron', 1234567890, 'Rom', 'Myk', 970000000, 'avatar');
-// console.log(user);
-
-// Об'єкт не ітерабельний об'єкт. Тут буде помилка!!!
-// for (let key of user) {
-//     console.log(key);
-// }
-
-const entriesUser = Object.entries(user);
-
-for (let key of entriesUser) {
-    console.log(key);
-}
-
-const fromEntriesUser = Object.fromEntries(entriesUser);
-fromEntriesUser.login = 'lapulja';
-console.log(fromEntriesUser);
-console.log(user);
-
-
-//
-// const array = [1, 2, 3, 'Hello'];
-//
-// for (let key in array) {
-//     console.log(key);
-// }
+//1
+// 2
+// 3
+// 4
+// { name: 'Roman' }
+// undefined
+// Hello
